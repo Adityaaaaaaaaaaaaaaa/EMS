4436aafb311a79997446d0f95f2ec37c90258353
Aliyah <173361210+Aliyah08@users.noreply.github.com>
2024-08-22 15:08:38 +0400

    Organizer profile

--- src/main/java/gui/Organizer_profile.java
+++ src/main/java/gui/Organizer_profile.java
@@ -5,4 +5,11 @@ import javax.swing.*;
 public class Organizer_profile {
     private JPanel main_panel;
     private JPanel Menu;
+    private JTextField OrgName;
+    private JTextField OrgUname;
+    private JTextField OrgEmail;
+    private JTextField OrgPwd;
+    private JTextField OrgAdd;
+    private JTextField textField1;
+    private JLabel OrgPhoneNum;
 }
